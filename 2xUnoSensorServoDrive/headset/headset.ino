227b8221a4af9e7d2e282c01841fcc5342157be5
Aidan Boyer <nordithen@gmail.com>
2022-03-23 00:15:07 -0400

    Incorporating color sensor and rover driving together

---
+++ 2xUnoSensorServoDrive/headset/headset.ino
@@ -0,0 +1,120 @@
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_180MS, TCS34725_GAIN_60X);
+
+//  TCS34725_GAIN_1X = 0x00,  /**<  No gain  */
+//  TCS34725_GAIN_4X = 0x01,  /**<  4x gain  */
+//  TCS34725_GAIN_16X = 0x02, /**<  16x gain */
+//  TCS34725_GAIN_60X = 0x03  /**<  60x gain */
+
+int zeroSetJumperPin = 3;
+int resetIsPressed = 0;
+int rightMotorPowerSignalPin = 5;
+int leftMotorPowerSignalPin = 6;
+float rRaw, gRaw, bRaw;
+float rZero, gZero, bZero;
+float r, g, b;
+float gbDifference;
+float gbNeutralDifference;
+float gbDifferenceMax = 50;
+float gbDifferenceMin = 50;
+float forwardRMaxRange = 20;
+float reverseRMaxRange = 20;
+float forwardSpeed;
+float turningRate;
+float rightMotorPower;
+float leftMotorPower;
+float turningSensitivity = 0.25;
+
+void setup() {
+  Wire.begin(); // join i2c bus (address optional for master)
+  pinMode(zeroSetJumperPin, INPUT);
+  Serial.begin(9600);
+
+  if (tcs.begin()) {
+    Serial.println("Found sensor");
+  } else {
+    Serial.println("No TCS34725 found ... check your connections");
+    while (1);
+  }
+
+  tcs.setInterrupt(true); // turn off LED - set to false to turn on LED
+
+  delay(1000);
+  
+  tcs.getRGB(&rRaw, &gRaw, &bRaw); // read RGB values from the color sensor
+  resetCenter(rRaw, gRaw, bRaw);
+}
+
+void loop() {
+  tcs.getRGB(&rRaw, &gRaw, &bRaw); // read RGB values from the color sensor
+
+  resetIsPressed = digitalRead(zeroSetJumperPin);
+  if (resetIsPressed) {resetCenter(rRaw, gRaw, bRaw);}
+
+  r = rRaw - rZero;
+  g = gRaw - gZero;
+  b = bRaw - bZero;
+  gbDifference = gRaw - bRaw;
+  
+  if (r > 0) {
+    forwardSpeed = (-r)/(forwardRMaxRange);
+  }
+  else {
+    forwardSpeed = (-r)/(reverseRMaxRange);
+  }
+
+  if ((gbDifference - gbNeutralDifference) > 0) {
+    turningRate = (gbDifference - gbNeutralDifference)/(gbDifferenceMax);
+  }
+  else {
+    turningRate = (gbDifference - gbNeutralDifference)/(gbDifferenceMin);
+  }
+
+  //Serial.print("R: "); Serial.print(rRaw, DEC); Serial.print(" ");
+  //Serial.print("G: "); Serial.print(gRaw, DEC); Serial.print(" ");
+  //Serial.print("B: "); Serial.print(bRaw, DEC); Serial.print(" ");
+  Serial.print("Forward: "); Serial.print(forwardSpeed, DEC); Serial.print(" ");
+  Serial.print("Turning: "); Serial.print(turningRate, DEC); Serial.print(" ");
+  Serial.println(" ");
+  
+
+  //transmitDrivingInstructions(forwardSpeed, turningRate);
+}
+
+void transmitMotorSpeeds(int right, int left) {
+  Wire.beginTransmission(4); // transmit to device #4
+
+  static String rightString;
+  static String leftString;
+  static String outputString;
+  static char outputArray[9];
+
+  //dtostrf(right, 4, 0, rightString);
+  //dtostrf(left, 4, 0, leftString);
+  rightString = String(right);
+  leftString = String(left);
+  outputString = rightString + ',' + leftString;
+  outputString.toCharArray(outputArray,9);
+  
+  Wire.write(outputArray);
+  Wire.endTransmission();    // stop transmitting
+
+  //delay(10);
+}
+
+void resetCenter(float rCenter, float gCenter, float bCenter) {
+  rZero = rCenter;
+  gZero = gCenter;
+  bZero = bCenter;
+  gbNeutralDifference = gZero - bZero;
+}
+
+void transmitDrivingInstructions(float forward, float turning) {
+  // This function assumes forwardSpeed and turningRate vary between -1 and 1
+  rightMotorPower = forwardSpeed - (turningRate * turningSensitivity);
+  leftMotorPower = forwardSpeed + (turningRate * turningSensitivity);
+  //analogWrite(128*(1+rightMotorPowerSignalPin), rightMotorPower);
+  //analogWrite(128*(1+leftMotorPowerSignalPin), leftMotorPower);
+}
